462c40abf5779165afd20588647c904523de5f6a
Ricardo Marques <ricardo@sunflower-labs.com>
2025-07-04 11:45:07 +0200

    Increase DEFAULT_CACHE_RESERVES, so we can subscribe to more topics

--- modules/ipc/include/hephaestus/ipc/zenoh/liveliness.h
+++ modules/ipc/include/hephaestus/ipc/zenoh/liveliness.h
@@ -75,7 +75,7 @@ private:
 
   std::unique_ptr<::zenoh::Subscriber<void>> liveliness_subscriber_;
 
-  static constexpr std::size_t DEFAULT_CACHE_RESERVES = 1000;
+  static constexpr std::size_t DEFAULT_CACHE_RESERVES = 1500;
   std::unique_ptr<concurrency::MessageQueueConsumer<EndpointInfo>> infos_consumer_;
 };
 
